ef800ec4f2a7743fc838113cbca85ceac8285f60
dev1week <rondo2860@gmail.com>
2023-03-23 20:51:46 +0900

    #1 -3 에러페이지 등록

---
+++ src/main/java/com/example/exception/WebServerCustomizer.java
@@ -0,0 +1,19 @@
+package com.example.exception;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+@Component
+public class WebServerCustomizer implements
+        WebServerFactoryCustomizer<ConfigurableWebServerFactory>{
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEX = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        //팩토리에 에러 페이지를 등록한다.
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEX);
+    }
+}
