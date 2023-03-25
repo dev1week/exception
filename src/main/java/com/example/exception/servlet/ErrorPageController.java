b49c3e67adc55d2118d08fa55758134da098a043
dev1week <rondo2860@gmail.com>
2023-03-25 14:03:11 +0900

    #3 -2 서블릿에서의 api 에러 처리

--- src/main/java/com/example/exception/servlet/ErrorPageController.java
+++ src/main/java/com/example/exception/servlet/ErrorPageController.java
@@ -1,11 +1,19 @@
 package com.example.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Slf4j
 @Controller
@@ -30,6 +38,23 @@ public class ErrorPageController {
         log.info("errorPage 500");
         printErrorInfo(request);
         return "error-page/500";
+    }
+
+
+    //클라이언트가 json만 받을 수 잇도록 헤더에 accept를 application/json으로 설정했을 경우 해당 메서드로 라우팅한다.
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPapge500api(
+        HttpServletRequest request, HttpServletResponse response){
+        log.info("api error page 500");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return new ResponseEntity(result, HttpStatus.valueOf(statusCode));
+
     }
     private void printErrorInfo(HttpServletRequest request) {
         log.info("ERROR_EXCEPTION: ex=", request.getAttribute(ERROR_EXCEPTION));
