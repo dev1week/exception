661604251634aef1c0faa41c55eda05c42a0afd4
dev1week <rondo2860@gmail.com>
2023-03-25 16:03:26 +0900

    #8 -4 DefaultHandlerExceptionResolver

--- src/main/java/com/example/exception/Api/ApiExceptionController.java
+++ src/main/java/com/example/exception/Api/ApiExceptionController.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 
@@ -40,6 +41,11 @@ public class ApiExceptionController {
     @GetMapping("/api/response-status-ex2")
     public String responseStatusEx2(){
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new  IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data){
+        return "ok";
     }
 
     @Data
