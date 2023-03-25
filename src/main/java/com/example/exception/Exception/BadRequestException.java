d9906714866e09522fd0a1ff45670ba90c27b8e6
dev1week <rondo2860@gmail.com>
2023-03-25 15:35:27 +0900

    #8 -2 @ResponseStatus 메시지 소스 사용법

--- src/main/java/com/example/exception/Exception/BadRequestException.java
+++ src/main/java/com/example/exception/Exception/BadRequestException.java
@@ -2,7 +2,7 @@ package com.example.exception.Exception;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
-@ResponseStatus(code= HttpStatus.BAD_REQUEST, reason = "잘못된 요청 오류")
+@ResponseStatus(code= HttpStatus.BAD_REQUEST, reason = "error.bad")
 public class BadRequestException extends RuntimeException{
 
 }
